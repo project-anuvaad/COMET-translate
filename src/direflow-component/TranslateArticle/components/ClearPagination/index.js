bc48b280e499ecaba89ff95eb6c49119aacb6732
hassan <hassanmamin994@gmail.com>
2020-03-01 00:27:07 +0200

    fix pagination UI

--- src/direflow-component/TranslateArticle/components/ClearPagination/index.js
+++ src/direflow-component/TranslateArticle/components/ClearPagination/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Pagination, Icon } from 'semantic-ui-react'
 
-import { Styled } from 'direflow-component';
+import { withStyles } from 'direflow-component';
 import styles from './style.scss';
 
-export default class ClearPagination extends React.Component {
+class ClearPagination extends React.Component {
     render() {
         const {
             activePage,
@@ -13,19 +13,19 @@ export default class ClearPagination extends React.Component {
         } = this.props;
 
         return (
-            <Styled styles={styles}>
-                <Pagination
-                    activePage={activePage}
-                    onPageChange={onPageChange}
-                    totalPages={totalPages}
-                    firstItem={null}
-                    // id="videos-pagination"
-                    className='clear-pagination'
-                    lastItem={null}
-                    prevItem={{ content: <div><Icon name='angle left' /> Previous</div> }}
-                    nextItem={{ content: <div>Next <Icon name='angle right' /></div> }}
-                />
-            </Styled>
+            <Pagination
+                activePage={activePage}
+                onPageChange={onPageChange}
+                totalPages={totalPages}
+                firstItem={null}
+                // id="videos-pagination"
+                className='clear-pagination'
+                lastItem={null}
+                prevItem={{ content: <div><Icon name='angle left' /> Previous</div> }}
+                nextItem={{ content: <div>Next <Icon name='angle right' /></div> }}
+            />
         )
     }
 }
+
+export default withStyles(styles)(ClearPagination)
