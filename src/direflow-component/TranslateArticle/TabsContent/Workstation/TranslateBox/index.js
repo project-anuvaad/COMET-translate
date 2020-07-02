6da2ef3d6563bce62e462e570e3a2a97421c0bd4
Osama Hamed <osamahamedmohammed@gmail.com>
2020-07-02 11:24:53 +0200

    restore textarea

--- src/direflow-component/TranslateArticle/TabsContent/Workstation/TranslateBox/index.js
+++ src/direflow-component/TranslateArticle/TabsContent/Workstation/TranslateBox/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Button, Popup, Card, Label, Icon } from 'semantic-ui-react';
+import { Button, Popup, Card, Label, Icon, TextArea } from 'semantic-ui-react';
 import { debounce } from '../../../utils/helpers';
 import FindAndReplaceModal from '../../../components/FindAndReplaceModal';
 import { Styled } from 'direflow-component';
 import { CircularProgressBar, buildStyles } from '../../../components/CircularProgressBar';
-import stripHtml from "string-strip-html";
-import { Editor } from '@tinymce/tinymce-react';
 import styles from './style.scss';
-import { off } from 'superagent';
 
 
 const defaultWordsPerMinute = 120;
@@ -40,10 +37,7 @@ class TranslateBox extends React.Component {
     }
 
     componentDidMount() {
-        const langWordsPerMinute = langsWordsPerMinute.find((l) => l.code === this.props.langCode);
-        const wordsPerMinute = langWordsPerMinute ? langWordsPerMinute.limit : defaultWordsPerMinute;
-        const wordLimit = Math.round(wordsPerMinute / 60 * this.props.duration);
-        this.setState({ wordLimit });
+        this.setState({ wordLimit: this.getWordLimit(this.props.langCode, this.props.duration) });
 
         if (this.state.value !== this.props.value) {
             this.setState({ value: this.props.value });
@@ -51,83 +45,30 @@ class TranslateBox extends React.Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        const langWordsPerMinute = langsWordsPerMinute.find((l) => l.code === nextProps.langCode);
-        const wordsPerMinute = langWordsPerMinute ? langWordsPerMinute.limit : defaultWordsPerMinute;
-        const wordLimit = Math.round(wordsPerMinute / 60 * nextProps.duration);
-        this.setState({ wordLimit });
+        this.setState({ wordLimit: this.getWordLimit(nextProps.langCode, nextProps.duration) });
 
         if (this.props.value !== nextProps.value) {
             if ((this.props.currentSlideIndex !== nextProps.currentSlideIndex || this.props.currentSubslideIndex !== nextProps.currentSubslideIndex) && this.props.value !== this.state.value) {
-                this.props.onSave(stripHtml(this.state.value), this.props.currentSlideIndex, this.props.currentSubslideIndex);
+                this.props.onSave(this.state.value, this.props.currentSlideIndex, this.props.currentSubslideIndex);
             }
             this.setState({ value: nextProps.value });
         }
     }
 
-    onValueChange = (value, editor, currentSlideIndex, currentSubslideIndex) => {
-        console.log(editor);
-        
-        // console.log(editor.selection.getRng().startOffset);
-        // console.log(editor.selection.getRng().endOffset);
-        // console.log(editor.selection.getNode());
-
-        // console.log(value);
-        
-        const node = editor.selection.getNode(); 
-        const offset = editor.selection.getRng().startOffset;
-        console.log(node, offset);
-        
-        editor.selection.setCursorLocation(node, offset);
-        editor.setContent(this.getHighlightedText(editor.getContent({ format: 'text' }), this.state.wordLimit), { format: 'html' });
-
-        // this.setState({ value: editor.getContent({ format: 'text' }) });
-        // editor.setContent(this.getHighlightedText(editor.getContent({ format: 'text' }), this.state.wordLimit), { format: 'html' });
-        
-        // console.log(`"${editor.getContent({ format: 'text' })}"`);
-        // console.log(`"${this.state.value}"`);
-        
-
-        // if (editor.getContent({ format: 'text' }) !== this.state.value) {
-            // this.setState({ value: editor.getContent({ format: 'text' }) });
-            // const bm = editor.selection.getBookmark();
-            // editor.setContent(this.getHighlightedText(editor.getContent({ format: 'text' }), this.state.wordLimit), { format: 'html' });
-            // editor.selection.moveToBookmark(bm);
-        // }
-        
-
-        // if (stripHtml(value) !== stripHtml(this.state.value)) {
-        //     this.setState({ value }, () => {
-        //         const bm = editor.selection.getBookmark();
-        //         editor.setContent(editor.getContent() + 'Some new content');
-        //         editor.selection.moveToBookmark(bm);
-        //     });
-            
-
-        //     // console.log(bm);
-        //     // editor.selection.moveToBookmark(bm);
-        
-        //     // // console.log(x);
-            
-        //     // this.setState({ value: this.getHighlightedText(value, this.state.wordLimit) }, () => {
-        //     //     editor.selection.moveToBookmark(bm);
-        //     //     // editor.selection.setCursorLocation(editor.selection.getNode(), editor.selection.getRng().startOffset);
-        //     //     // editor.selection.setCursorLocation(node, offset);
-        //     // });
-        // }
+    onValueChange = (value, currentSlideIndex, currentSubslideIndex) => {
+        this.setState({ value });
+        // this.saveValue(value, currentSlideIndex, currentSubslideIndex);
     }
+
+    getWordLimit = (langCode, duration) => {
+        const langWordsPerMinute = langsWordsPerMinute.find((l) => l.code === langCode);
+        const wordsPerMinute = langWordsPerMinute ? langWordsPerMinute.limit : defaultWordsPerMinute;
+        return Math.round(wordsPerMinute / 60 * duration);
+    }
+
 
     getWordCount = () => {
-        const count = stripHtml(this.state.value).split(' ').filter(v => v).length;
-        return count;
-    }
-
-    getHighlightedText = (value, wordLimit) => {
-        const valueArray = value.split(' ').filter(v => v);
-        if (valueArray.length <= wordLimit) {
-            return value;
-        }
-        const highlightedTextArray = valueArray.splice(wordLimit);
-        return `${valueArray.join(' ')} <span style="background: rgba(249, 157, 37, 0.2);">${highlightedTextArray.join(' ')}</span>`;
+        return this.state.value.split(' ').filter(v => v).length;
     }
 
     render() {
@@ -141,6 +82,7 @@ class TranslateBox extends React.Component {
                     <Card.Header style={{ backgroundColor: '#d4e0ed', color: '', borderRadius: 0 }}>
                         <div
                             style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+                            className="anything"
                         >
                             <h4 style={{ color: '#333333', margin: 0, padding: '1rem' }}>
                                 {title}
@@ -150,7 +92,7 @@ class TranslateBox extends React.Component {
                                     basic
                                     className="translate-box__update-button"
                                     loading={loading}
-                                    // disabled={loading || value.trim() === this.props.value.trim() || !value.trim()}
+                                    disabled={loading || value.trim() === this.props.value.trim() || !value.trim()}
                                     style={{ backgroundColor: 'transparent', boxShadow: 'none !important', margin: 0, padding: '1rem' }}
                                     onClick={() => this.props.onSave(value, this.props.currentSlideIndex, this.props.currentSubslideIndex)}
                                 >
@@ -160,7 +102,7 @@ class TranslateBox extends React.Component {
                         </div>
                     </Card.Header>
                     <div
-                        style={{ margin: 0, padding: 0, position: 'relative', display: 'flex', flexDirection: 'column' }}
+                        style={{ margin: 0, padding: 0, position: 'relative' }}
                     >
                         {!this.props.disabled && (
                             <React.Fragment>
@@ -236,88 +178,14 @@ class TranslateBox extends React.Component {
                             </React.Fragment>
                         )}
 
-                        {/* <TextArea
-                            style={{ padding: 20, paddingRight: 40, width: '100%', border: 'none', cursor: this.props.disabled ? 'not-allowed' : 'text' }}
+                        <TextArea
+                            style={{ padding: 20, paddingRight: 40, marginBottom: 40, maxHeight: 100, width: '100%', border: 'none', resize: 'none', cursor: this.props.disabled ? 'not-allowed' : 'text' }}
                             disabled={this.props.disabled}
                             rows={6}
                             placeholder="Translate slide text"
                             value={value}
                             onChange={(e, { value }) => { this.onValueChange(value, this.props.currentSlideIndex, this.props.currentSubslideIndex) }}
-                        /> */}
-
-                        {/* <ContentEditable
-                            style={{ padding: '20px 40px 50px 20px', height: 'auto', width: '100%', border: 'none', cursor: this.props.disabled ? 'not-allowed' : 'text' }}
-                            className="editable"
-                            tagName="div"
-                            html={value}
-                            disabled={this.props.disabled}
-                            onChange={(e) => { this.onValueChange(e.target.value, this.props.currentSlideIndex, this.props.currentSubslideIndex) }}
-                        /> */}
-
-                        {/* <Editor 
-                            theme={false}
-                            value={value}
-                            placeholder="Translate slide text"
-                            onChange={(html) => { 
-                                console.log('html', html);
-                                console.log('strip', `"${stripHtml(html)}"`);
-                                
-                                
-                                this.onValueChange(html, this.props.currentSlideIndex, this.props.currentSubslideIndex) 
-                            }}
-                        /> */}
-
-                        {/* <TextEditor
-                            toolbarHidden={true}
-                            editorState={this.state.editorState}
-                            onEditorStateChange={(editorState) => {
-                                    // const rawContentState = convertToRaw(this.state.editorState.getCurrentContent());
-                                    // const markup = draftToHtml(rawContentState);
-                                    // if (stripHtml(markup) === this.state.markup) {
-                                    //     return;
-                                    // }
-                                
-                                    // this.setState({ editorState: x }, () => {
-
-                                        // const markupArray = stripHtml(markup).split(' ');
-                                        // const colored = markupArray.splice(markupArray.length - 1);
-                                        // const contentBlock = htmlToDraft(`${markupArray.join(' ')} <span style="color: red">${colored.join(' ')}</span>`);
-                                        // if (contentBlock) {
-                                        //     const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
-                                        //     const editorState = EditorState.createWithContent(contentState);
-                                        //     this.setState({ editorState: editorState, markup: stripHtml(markup) }, () => {
-                                        //         this.setState({ editorState: x });
-                                        //     });
-                                        // }
-                                    // });
-                            }}
-                        /> */}
-
-                        <Editor
-                            initialValue={this.state.value}
-                            // value={this.state.value}
-                            disabled={this.props.disabled}
-                            init={{
-                                menubar: false,
-                                plugins: "autoresize",
-                                toolbar: false,
-                                resize: false,
-                                branding: false,
-                                statusbar: false,
-                                placeholder: "Translate slide text",
-                                // setup: function (ed) {
-                                //     ed.on('NodeChange', function(e) {
-                                //         console.log('changed');
-                                //         ed.selection.setCursorLocation(ed.selection.getNode(), ed.selection.getRng().startOffset);
-                                        
-                                //     });
-                                // }
-                              }}
-                              onEditorChange={(value, editor) => {
-                                this.onValueChange(value, editor, this.props.currentSlideIndex, this.props.currentSubslideIndex);
-                              }}
                         />
-
 
                         {/* {this._renderSlideTranslateBox()} */}
                     </div>
